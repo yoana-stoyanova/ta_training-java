fed08ace8af9a54806cd8dad27ff78856ee8ef5b
yoana stoyanova <jstoyanova14.05@gmail.com>
2025-06-22 18:48:20 +0300

    junit branch version

--- src/test/java/com/epam/training/yoana_stoyanova/PastebinHomePageTest.java
+++ src/test/java/com/epam/training/yoana_stoyanova/PastebinHomePageTest.java
@@ -4,24 +4,39 @@ import com.epam.training.yoana_stoyanova.PastebinHomePage;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class PastebinHomePageTest {
-    public static void main(String[] args) {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
 
-        try {
-            PastebinHomePage homePage = new PastebinHomePage(driver);
-            homePage.open();
+    private WebDriver driver;
 
-            homePage.codeField().sendKeys("Hello from WebDriver");
-            homePage.expirationDropdown().click();
-            homePage.expirationOption10Min().click();
-            homePage.titleField().sendKeys("helloweb");
-            homePage.createPasteButton().click();
+    @Before
+    public void setUp() {
+        WebDriverManager.chromedriver().clearResolutionCache().driverVersion("137.0.7151.120").setup();
+        driver = new ChromeDriver();
+    }
 
-        } finally {
+    @Test
+    public void testCreatePaste() {
+        PastebinHomePage homePage = new PastebinHomePage(driver);
+        homePage.open();
+
+        homePage.codeField().sendKeys("Hello from WebDriver");
+        homePage.expirationDropdown().click();
+        homePage.expirationOption10Min().click();
+        homePage.titleField().sendKeys("helloweb");
+        homePage.createPasteButton().click();
+
+        // You can add assertions here to verify results
+    }
+
+    @After
+    public void tearDown() {
+        if (driver != null) {
             driver.quit();
         }
     }
